73702857b2a0fca6e87bd737c2649d2c2b633964
Donald Chinhuru <donychinhuru@gmail.com>
2025-12-09 16:22:24 +0200

    drop emulator

--- emulator/mobile-preview/src/app/constants/endpoints.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL: string = 'http://192.168.0.57:8080/';
-
-export const API_ENDPOINTS = {
-  send_response: API_URL + 'send',
-};
--- emulator/node-server/app/v1.0.0/modules/whatsapp/enums/recipient.enum.js
+++
@@ -1,6 +0,0 @@
-const RecipientEnum = {
-    WHATSAPP_ENGINE: 'whatsapp_engine',
-    MOBILE_PHONE: 'mobile_phone'
-}
-
-module.exports = RecipientEnum;
